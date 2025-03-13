99598421af48ce6961aedbc146232713fa80b777
shuflduf <shuflduf@gmail.com>
2025-03-13 19:54:57 +0000

    added subcomand

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,42 @@
-use inquire::{error::InquireError, Select, Text};
+use clap::{Parser, Subcommand};
+use inquire::{Select, Text};
 
 mod versions;
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Init,
+}
+
+fn init_server() {
+    let name = Text::new("Server Name: ").prompt().unwrap();
+    let version = Select::new("Minecraft Version: ", versions::get_versions())
+        .prompt()
+        .unwrap();
+    let loader = Select::new(
+        "Loader: ",
+        vec!["Vanilla", "Fabric", "Forge", "NeoForge", "Quilt"],
+    )
+    .prompt()
+    .unwrap();
+    println!(
+        "Server Name: {}, Version: {}, Loader: {}",
+        name, version, loader
+    );
+}
+
 fn main() {
-    let name = Text::new("Server Name: ").prompt();
-    let version = Select::new("Minecraft Version: ", versions::get_versions()).prompt();
-    let loader = Select::new("Loader: ", vec!["Vanilla", "Fabric", "Forge", "NeoForge", "Quilt"]).prompt();
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Init => init_server(),
+    }
 }
