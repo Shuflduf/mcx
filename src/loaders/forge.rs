1411a0a42ee5341e2eb3cd27fbfe85fe48d037a1
Shuflduf <shuflduf@shuflduf.xyz>
2025-09-05 10:45:29 -0600

    forge done

--- src/loaders/forge.rs
+++ src/loaders/forge.rs
@@ -1,8 +1,8 @@
-use std::cmp::Ordering;
+use std::{cmp::Ordering, collections::HashMap};
 
 use color_eyre::eyre::Result;
 
-use crate::loaders::MCLoader;
+use crate::{init, loaders::MCLoader};
 
 #[derive(Default)]
 pub struct ForgeLoader {
@@ -16,8 +16,32 @@ impl MCLoader for ForgeLoader {
         let version_keys: Vec<String> = versions_map.keys().cloned().collect();
         let game_versions = remove_tags(version_keys);
         self.game_version = inquire::Select::new("Version", game_versions).prompt()?;
+        self.loader_version = inquire::Select::new(
+            "Loader Version",
+            filter_loader_versions(&versions_map, &self.game_version),
+        )
+        .prompt()?;
         Ok(())
     }
+    fn download_server_jar(&mut self) -> Result<()> {
+        let jar_url = format!(
+            "https://maven.minecraftforge.net/net/minecraftforge/forge/{}-{}/forge-{}-{}-installer.jar",
+            self.game_version, self.loader_version, self.game_version, self.loader_version
+        );
+        println!("{jar_url}");
+        init::download_server_file(jar_url)?;
+        Ok(())
+    }
+}
+
+fn filter_loader_versions(versions_map: &HashMap<String, String>, version: &String) -> Vec<String> {
+    let mut versions: Vec<String> = versions_map
+        .iter()
+        .filter(|(game_ver, _)| game_ver.starts_with(version))
+        .map(|(_, loader_ver)| loader_ver.to_string())
+        .collect();
+    versions.dedup();
+    versions
 }
 
 fn remove_tags(versions: Vec<String>) -> Vec<String> {
@@ -32,6 +56,7 @@ fn remove_tags(versions: Vec<String>) -> Vec<String> {
     versions
 }
 
+#[allow(clippy::ptr_arg)]
 fn sort_versions(a: &String, b: &String) -> Ordering {
     let a_segments = a.split('.').map(|s| s.parse::<u32>().unwrap_or(0));
     let b_segments = b.split('.').map(|s| s.parse::<u32>().unwrap_or(0));
