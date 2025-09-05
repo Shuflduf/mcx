30fb50d85bcd602f5c25d83db54c7adf5bd3ef40
Shuflduf <shuflduf@shuflduf.xyz>
2025-09-05 09:55:51 -0600

    sorting mc versions

--- src/loaders/forge.rs
+++ src/loaders/forge.rs
@@ -1,7 +1,6 @@
-use std::collections::HashMap;
+use std::cmp::Ordering;
 
 use color_eyre::eyre::Result;
-use serde::Deserialize;
 
 use crate::loaders::MCLoader;
 
@@ -16,7 +15,7 @@ impl MCLoader for ForgeLoader {
         let versions_map = version_getter::get_versions()?;
         let version_keys: Vec<String> = versions_map.keys().cloned().collect();
         let game_versions = remove_tags(version_keys);
-        dbg!(game_versions);
+        self.game_version = inquire::Select::new("Version", game_versions).prompt()?;
         Ok(())
     }
 }
@@ -25,13 +24,29 @@ fn remove_tags(versions: Vec<String>) -> Vec<String> {
     let mut versions: Vec<String> = versions
         .iter()
         .map(|v| v.rsplit_once("-").unwrap().0.to_string())
+        .filter(|v| !v.contains("_"))
         .collect();
+    versions.sort_by(sort_versions);
     versions.dedup();
+    versions.reverse();
     versions
 }
 
+fn sort_versions(a: &String, b: &String) -> Ordering {
+    let a_segments = a.split('.').map(|s| s.parse::<u32>().unwrap_or(0));
+    let b_segments = b.split('.').map(|s| s.parse::<u32>().unwrap_or(0));
+    for (a_val, b_val) in a_segments.zip(b_segments) {
+        match a_val.cmp(&b_val) {
+            Ordering::Less => return Ordering::Less,
+            Ordering::Greater => return Ordering::Greater,
+            Ordering::Equal => continue,
+        }
+    }
+    a.len().cmp(&b.len())
+}
+
 mod version_getter {
-    use std::{collections::HashMap, fs::File, io::Read};
+    use std::collections::HashMap;
 
     use color_eyre::eyre::Result;
     use serde::Deserialize;
